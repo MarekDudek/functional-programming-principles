b23e8c0e0b1d34707bddf989abcd81ae83b87fbe
Marek Dudek <marek.dudek@gmail.com>
2015-07-27 22:38:11 +0200

    Nested functions inside function and got rid of obsolete parameter passing

--- src/biz/interretis/functions_and_evaluations/SquareRootNewtonMethod.scala
+++ src/biz/interretis/functions_and_evaluations/SquareRootNewtonMethod.scala
@@ -2,15 +2,20 @@ package biz.interretis.functions_and_evaluations
 
 object SquareRootNewtonMethod extends App {
 
-  def sqrtIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
+  def sqrt(x: Double) = {
 
-  def isGoodEnough(guess: Double, x: Double) =
-    Math.abs(guess * guess - x) / x < 0.001
+    def sqrtIter(guess: Double): Double = {
 
-  def improve(guess: Double, x: Double) =
-    (guess + x / guess) / 2
+      def isGoodEnough(guess: Double) =
+        Math.abs(guess * guess - x) / x < 0.001
 
-  def sqrt(x: Double) = sqrtIter(1.0, x)
+      def improve(guess: Double) =
+        (guess + x / guess) / 2
+
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
+    }
+
+    sqrtIter(1.0)
+  }
 }
