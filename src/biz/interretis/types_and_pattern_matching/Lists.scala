f3346d68b1f02b092d6c11c023d00adb79d6aa2d
Marek Dudek <marek.dudek@gmail.com>
2015-08-02 22:08:39 +0200

    Insertion sort little more readable

--- src/biz/interretis/types_and_pattern_matching/Lists.scala
+++ src/biz/interretis/types_and_pattern_matching/Lists.scala
@@ -20,8 +20,10 @@ object Lists extends App {
   def insert(x: Int, xs: List[Int]): List[Int] = xs match {
     case Nil => List(x)
     case y :: ys =>
-      if (x < y) x :: xs
-      else y :: insert(x, ys)
+      if (x < y) 
+        x :: y :: ys
+      else 
+        y :: insert(x, ys)
   }
 
   def isort(xs: List[Int]): List[Int] = xs match {
