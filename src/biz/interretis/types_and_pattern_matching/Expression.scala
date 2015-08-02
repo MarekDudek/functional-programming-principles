b31628219f6bf96728e57238080f3a56403fa993
Marek Dudek <marek.dudek@gmail.com>
2015-08-02 21:00:55 +0200

    Evaluation and showing using pattern matching

--- src/biz/interretis/types_and_pattern_matching/Expression.scala
+++ src/biz/interretis/types_and_pattern_matching/Expression.scala
@@ -2,28 +2,32 @@ package biz.interretis.types_and_pattern_matching
 
 trait Expression {
 
-  def eval: Int
-}
+  def eval: Int = this match {
+    case Number(n)             => n
+    case Sum(Number(0), e)     => e.eval
+    case Sum(e1, e2)           => e1.eval + e2.eval
+    case Product(Number(1), e) => e.eval
+    case Product(e1, e2)       => e1.eval * e2.eval
+  }
 
-class Number(val number: Int) extends Expression {
-
-  def eval = number
-}
-
-class Sum(val leftOp: Expression, val rightOp: Expression) extends Expression {
-
-  def eval = leftOp.eval + rightOp.eval
-}
-
-object Expression {
-
-  def eval(expression: Expression): Int = {
-    if (expression.isInstanceOf[Number])
-      expression.asInstanceOf[Number].number
-    else if (expression.isInstanceOf[Sum])
-      eval(expression.asInstanceOf[Sum].leftOp) +
-        eval(expression.asInstanceOf[Sum].rightOp)
-    else
-      throw new Error("unknown expression: " + expression)
+  def show: String = this match {
+    case Number(n)                               => n.toString
+    case Sum(e1, e2)                             => e1.show + " + " + e2.show
+    case Product(n @ Number(_), sum @ Sum(_, _)) => n.show + " * (" + sum.show + ")"
+    case Product(sum @ Sum(_, _), n @ Number(_)) => "(" + sum.show + ") * " + n.show
+    case Product(e1, e2)                         => e1.show + " * " + e2.show
   }
 }
+
+case class Number(number: Int) extends Expression {
+
+}
+
+case class Sum(leftOp: Expression, rightOp: Expression) extends Expression {
+
+}
+
+case class Product(leftOp: Expression, rightOp: Expression) extends Expression {
+
+}
+
--- test/biz/interretis/types_and_pattern_matching/ExpressionSuite.scala
+++ test/biz/interretis/types_and_pattern_matching/ExpressionSuite.scala
@@ -7,12 +7,15 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExpressionSuite extends FunSuite {
 
-  import biz.interretis.types_and_pattern_matching.Expression._
-
   val two = new Number(2)
   val three = new Number(3)
   val five = new Sum(two, three)
   val nine = new Sum(five, new Number(4))
+  val twelve = new Product(three, new Product(two, two))
+
+  val sumWithProduct = Sum(Number(2), Product(Number(3), Number(4)))
+  val productWithSum = Product(Number(2), Sum(Number(3), Number(4)))
+  val productWithSum2 = Product(Sum(Number(3), Number(4)), Number(2))
 
   test("2 + 3 = 5") {
     assert(five.eval === 5)
@@ -20,5 +23,29 @@ class ExpressionSuite extends FunSuite {
 
   test("2 + 3 + 4 = 9") {
     assert(nine.eval === 9)
+  }
+
+  test("3 * (2*2) = 12") {
+    assert(twelve.eval === 12)
+  }
+
+  test("showing number") {
+    assert(two.show === "2")
+  }
+
+  test("showing sum") {
+    assert(Sum(Number(2), Number(3)).show === "2 + 3")
+    assert(Sum(Number(2), Sum(Number(3), Number(4))).show == "2 + 3 + 4")
+  }
+
+  test("showing product") {
+    assert(Product(Number(2), Number(3)).show === "2 * 3")
+    assert(Product(Number(2), Product(Number(3), Number(4))).show == "2 * 3 * 4")
+  }
+
+  test("showing sum and product") {
+    assert(sumWithProduct.show === "2 + 3 * 4")
+    assert(productWithSum.show === "2 * (3 + 4)")
+    assert(productWithSum2.show === "(3 + 4) * 2")
   }
 }
